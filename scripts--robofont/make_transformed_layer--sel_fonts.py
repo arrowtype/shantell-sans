8cfc17cbb2d4af9fdf52ebd3fdcdfc2e504af26a
arrowtype <arrowtypeco@gmail.com>
2020-08-30 12:52:12 -0400

    make experimental UFOs with glyphs transformed in a background layer

--- scripts--robofont/make_transformed_layer--sel_fonts.py
+++ scripts--robofont/make_transformed_layer--sel_fonts.py
@@ -13,13 +13,13 @@ from vanilla.dialogs import *
 
 # if you provide a custom config path, this picks it up
 try:
-    configPath = '/Users/stephennixon/type-repos/shantell/sources/organic_experiment_04-alts-shift-shaping/transforms.yaml'
+    transformsPath = '/Users/stephennixon/type-repos/shantell/sources/organic_experiment_04-alts-shift-shaping/transforms.yaml'
 except IndexError:
     print("Sorry, the YAML path needs to be updated in this script.")
 
 # read yaml config
-with open(configPath) as file:
-    transforms = yaml.load(file, Loader=yaml.FullLoader)
+with open(transformsPath, 'r', encoding='utf8') as yml:
+    transforms = yaml.load(yml, Loader=yaml.FullLoader)
 
 # import pprint
 # pprint.pprint(transforms)
@@ -27,12 +27,32 @@ with open(configPath) as file:
 
 files = getFile("Select UFOs", allowsMultipleSelection=True, fileTypes=["ufo"])
 
+newLayer = "transformed"
+
 for file in files:
     f = OpenFont(file, showInterface=False)
 
     for g in f:
         if g.name in transforms.keys():
-            print(g.name, transforms[g.name])
+
+            g.layers[0].copyToLayer(newLayer, clear=True)
+
+            # apparently, better than matching `width`
+            g.getLayer(newLayer).leftMargin = g.layers[0].leftMargin
+            g.getLayer(newLayer).rightMargin = g.layers[0].rightMargin
+
+            transX, transY = transforms[g.name].split()[0].replace(",",""), transforms[g.name].split()[1]
+            transX, transY = int(transX)/100, int(transY)/100
+
+            g.getLayer(newLayer).transformBy((transX, 0, 0, transY, 0, 0), origin=(g.width/2, f.info.xHeight/2))
+
+            # # if you don’t want things duplexed....
+            # g.getLayer(newLayer).leftMargin = g.layers[0].leftMargin * transX
+            # g.getLayer(newLayer).rightMargin = g.layers[0].rightMargin * transX
+
+            # transform the layer width
+
+    # show 'transform' layer stroke, hide others
 
 
     f.save()
