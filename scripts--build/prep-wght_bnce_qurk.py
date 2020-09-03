b669aa935eca7c67bc3e1d3eb1cfe263c947baaa
arrowtype <arrowtypeco@gmail.com>
2020-09-02 20:26:02 -0400

    set up pseudocode for build-prep script

---
+++ scripts--build/prep-wght_bnce_qurk.py
@@ -0,0 +1,24 @@
+"""
+    1. Copy sources to new folder
+    2. Duplicate normal sources & run "bouncy" script on these, updating file & style names
+    3. Run "organic" script on quirk sources
+    4. copy features to UFOs
+"""
+
+# make folder 'wght_bnce_qurk'
+
+# copy in UFOs
+    # light
+    # extrabold
+    # organic-extrabold
+    # organic-light
+
+# copy normal sources
+    # bounce-light
+    # bounce-extrabold
+
+# make alts in all fonts
+
+# shift baseline in bounce fonts
+
+# interpolate alts in quirk fonts
