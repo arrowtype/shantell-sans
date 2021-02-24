55b6c5b3a5418858f483df5a0300c1f055a7a65d
arrowtype <arrowtypeco@gmail.com>
2021-02-24 12:54:26 -0500

    improve spacing of irregular sources; set font metric info

---
+++ scripts--robofont/set-font-info-sel_fonts.py
@@ -0,0 +1,45 @@
+from vanilla.dialogs import *
+
+inputFonts = getFile("select UFOs", allowsMultipleSelection=True, fileTypes=["ufo"])
+
+for fontPath in inputFonts:
+    f = OpenFont(fontPath, showInterface=False)
+
+    # for other font info attributes, see
+    # http://unifiedfontobject.org/versions/ufo3/fontinfo.plist/
+
+    f.info.openTypeOS2VendorID = "ARRW"
+
+    f.info.copyright = "Copyright 2021 The Shantell Sans Project Authors (https://github.com/arrowtype/shantell-sans)"
+
+    # setting metrics
+    ascent = 1020
+    descent = -320
+
+    f.info.openTypeOS2TypoAscender = ascent
+    f.info.openTypeOS2TypoDescender = descent
+    f.info.openTypeOS2TypoLineGap = 0
+    
+    f.info.openTypeOS2WinAscent = ascent
+    f.info.openTypeOS2WinDescent = abs(descent)
+
+    f.info.openTypeHheaAscender = ascent
+    f.info.openTypeHheaDescender = descent
+    f.info.openTypeHheaLineGap = 0
+
+    # un-nest nested components # https://github.com/googlefonts/fontbakery/issues/296
+    f.lib["com.github.googlei18n.ufo2ft.filters"] = [
+            {
+            'name': 'decomposeTransformedComponents', 
+            'pre': 1
+            },
+            {
+            'name': 'flattenComponents', 
+            'pre': 1
+            },
+    ]
+
+    print("Updated info for: ", f.info.familyName, " ", f.info.styleName)
+
+    f.save()
+    f.close()
