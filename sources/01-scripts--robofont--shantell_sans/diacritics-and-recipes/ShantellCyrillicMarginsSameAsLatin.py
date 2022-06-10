abea6904711b1e9fd779436dce5d10c016fceab4
sikeiros <hataptah@gmail.com>
2022-06-10 18:20:25 +0200

    organic extrabold cyrillic,  interpolatable all

---
+++ sources/01-scripts--robofont--shantell_sans/diacritics-and-recipes/ShantellCyrillicMarginsSameAsLatin.py
@@ -0,0 +1,143 @@
+f = CurrentFont()
+
+
+A = f['A']
+Acyr = f['Acyr']
+Acyr.rightMargin = A.rightMargin
+
+B = f['B']
+Vecyr = f['Vecyr']
+Vecyr.rightMargin = B.rightMargin
+
+E = f['E']
+Iecyr= f['Iecyr']
+Iocyr = f['Iocyr']
+Iecyr.rightMargin = E.rightMargin
+Iocyr.rightMargin = E.rightMargin
+
+K = f['K']
+Kacyr = f['Kacyr']
+Kacyr.rightMargin = K.rightMargin
+
+H = f['H']
+Encyr = f['Encyr']
+Encyr.rightMargin = H.rightMargin
+
+O = f['O']
+Ocyr = f['Ocyr']
+Ocyr.rightMargin = O.rightMargin
+
+P = f['P']
+Ercyr = f['Ercyr']
+Ercyr.rightMargin = P.rightMargin
+
+C = f['C']
+Escyr = f['Escyr']
+Escyr.rightMargin = C.rightMargin
+
+T = f['T']
+Tecyr = f['Tecyr']
+Tecyr.rightMargin = T.rightMargin
+
+X = f['X']
+Hacyr = f['Hacyr']
+Hacyr.rightMargin = X.rightMargin
+
+a = f['a']
+acyr = f['acyr']
+acyr.rightMargin = a.rightMargin
+
+e = f['e']
+iecyr = f['iecyr']
+iocyr = f['iocyr']
+iecyr.rightMargin = e.rightMargin
+iocyr.rightMargin = e.rightMargin
+
+o = f['o']
+ocyr = f['ocyr']
+ocyr.rightMargin = o.rightMargin
+
+p = f['p']
+ercyr = f['ercyr']
+ercyr.rightMargin = p.rightMargin
+
+c = f['c']
+escyr = f['escyr']
+escyr.rightMargin = c.rightMargin
+
+y = f['y']
+ucyr = f['ucyr']
+ucyr.rightMargin = y.rightMargin
+
+
+x = f['x']
+hacyr = f['hacyr']
+hacyr.rightMargin = x.rightMargin
+
+#---
+
+Iigrave-cy.loclBGR 
+De-cy.loclBGR 
+Ii-cy.loclBGR 
+Iishort-cy.loclBGR 
+El-cy.loclBGR
+
+Egrave = f['Egrave']
+Iegravecyr = f['Iegravecyr']
+Iegravecyr.rightMargin = Egrave.rightMargin
+
+S = f['S']
+Dzecyr = f['Dzecyr']
+Dzecyr.rightMargin = S.rightMargin
+
+I = f['I']
+Iukrcyr = f['Iukrcyr']
+Iukrcyr.rightMargin = I.rightMargin
+
+Idieresis = f['Idieresis']
+Yukrcyr = f['Yukrcyr']
+Yukrcyr.rightMargin = Idieresis.rightMargin
+
+J = f['J']
+Jecyr = f['Jecyr']
+Jecyr.rightMargin = J.rightMargin
+
+#---
+
+egrave = f['egrave']
+iegravecyr = f['iegravecyr']
+iegravecyr.rightMargin = egrave.rightMargin
+
+s = f['s']
+dzecyr = f['dzecyr']
+dzecyr.rightMargin = s.rightMargin
+
+i = f['i']
+iukrcyr = f['iukrcyr']
+iukrcyr.rightMargin = i.rightMargin
+
+idieresis = f['idieresis']
+yukrcyr = f['yukrcyr']
+yukrcyr.rightMargin = idieresis.rightMargin
+
+j = f['j']
+jecyr = f['jecyr']
+jecyr.rightMargin = j.rightMargin
+
+
+
+#-----------------------for italic
+
+# u = f['u']
+# icyr = f['icyr']
+# ishortcyr = f['ishortcyr']
+# icyr.rightMargin = u.rightMargin
+# ishortcyr.rightMargin = u.rightMargin
+
+# n = f['n']
+# pecyr = f['pecyr']
+# pecyr.rightMargin = n.rightMargin
+      
+
+
+#print(A.rightMargin)
