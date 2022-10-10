413e4e2873e8d5ed94a68d9b41964585b6d77f1f
arrowtype <arrowtypeco@gmail.com>
2022-10-10 14:03:47 -0400

    update release script to simplify release files

--- scripts--build/make-release.sh
+++ scripts--build/make-release.sh
@@ -5,10 +5,28 @@ version=$(cat "version.txt")
 fontDir="fonts/Shantell Sans"
 releaseDir="fonts/Shantell Sans $version"
 
+# clean up prior run
+rm -rf "$releaseDir"
+rm -rf ${releaseDir// /_}.zip
+
+# make new folder for release
 cp -r "$fontDir" "$releaseDir"
 
+# copy in supporting documents
 cp "scripts--build/release-data/ABOUT" "$releaseDir/1 - About"
 cp "OFL.txt" "$releaseDir/2 - License"
+
+# remove TTFs for simplicity
+rm -rf "$releaseDir/Desktop/Static/TTF"
+
+# move OTFs to top level of Desktop dir
+mv "$releaseDir/Desktop/Static/OTF" "$releaseDir/Desktop"
+
+# remove old Static dir
+rm -rf "$releaseDir/Desktop/Static"
+
+# change OTF folder name to simply Static
+mv "$releaseDir/Desktop/OTF" "$releaseDir/Desktop/Static"
 
 function zipit {
   currentDir=$(pwd)                             # get current dir so you can return later
