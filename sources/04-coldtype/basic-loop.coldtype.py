849455a568b4365cb3322866ff0f178755ffde93
Rob Stenson <rob.stenson@gmail.com>
2022-12-30 20:26:34 -0800

    experimental macro versions

--- sources/04-coldtype/basic-loop.coldtype.py
+++ sources/04-coldtype/basic-loop.coldtype.py
@@ -2,35 +2,37 @@ from coldtype import *
 
 fnt = Font.Find(r"ShantellSans\[.*\]\.ttf", regex_dir="fonts")
 
+VERSIONS = [
+    dict(script="latin", text="TYPOGRAPHY\nIS KINETIC", fontSize=225),
+    dict(script="cyrillic", text="ТИПОГРАФИЯ\nКИНЕТИЧЕСКАЯ", fontSize=180),
+] #/VERSIONS
 
-@animation((1920, 720), timeline=Timeline(50), bg=hsl(240,0,.94))
-def kinetic4(f, txt="TYPOGRAPHY\nIS KINETIC", fontSize=225):
+@animation((1920, 720)
+    , timeline=Timeline(50)
+    , bg=hsl(240,0,.94)
+    , release=lambda x: x.gifski()
+    , name="kinetic4_" + __VERSION__["script"]
+    )
+def kinetic4(f):
     def styler(g):
         if g.l == 0:
-            return Style(fnt, fontSize
+            return Style(fnt
+                , fontSize=__VERSION__["fontSize"]
                 , SPAC=f.e("eeio", 1, rng=(0, .25))
                 , wght=f.adj(-g.i*2).e("sio", 1, rng=(0, 1))
                 , ital=f.e("eeio", 1, rng=(0, 1))
                 , INFM=f.e("sio", 2, rng=(0, 1))
                 , BNCE=f.e("l", 1, rng=(0, 1)))
         else:
-            return Style(fnt, fontSize
+            return Style(fnt, __VERSION__["fontSize"]
                 , SPAC=f.e("eeio", 1, rng=(.4, 0))
                 , wght=f.adj(g.i*2).e("sio", 1, rng=(1, 0))
                 , ital=f.e("eeio", 1, rng=(1, 0))
                 , INFM=f.e("sio", 2, rng=(0, 1))
                 , BNCE=f.e("l", 1, rng=(1, 0)))
 
-    return (Glyphwise(txt, styler)
+    return (Glyphwise(__VERSION__["text"], styler)
         .xalign(f.a.r, tx=0)
         .lead(60)
         .align(f.a.r, tx=0)
         .f(0))
-
-
-@animation(**kinetic4.choose(["rect", "timeline", "bg"]))
-def kinetic4_cyrillic(f):
-    return kinetic4.func(f, txt="ТИПОГРАФИЯ\nКИНЕТИЧЕСКАЯ", fontSize=180)
-
-
-release = kinetic4.gifski(open=True)
--- sources/04-coldtype/bigger-loop.coldtype.py
+++ sources/04-coldtype/bigger-loop.coldtype.py
@@ -14,21 +14,18 @@ at = AsciiTimeline(2, 24, """
 0               1               2               3
 """).inflate(lines=[1])
 
-@animation((1400, 540), timeline=at, bg=1, render_bg=1)
+@animation((1400, 540), timeline=at, bg=1, render_bg=1, release=lambda x: x.gifski())
 def languages(f):
     current = at.current()
     txt = txts[int(current.name)]
 
     return (StSt(txt, fnt, 120
-        , SPAC=current.e("eeio", 1, rng=(0,0.25))
-        , wght=current.e("sio", 1, rng=(0, 1))
-        , ital=current.e("eeio", 1, rng=(0, 1))
-        , INFM=current.e("sio", 1, rng=(0, 1))
-        , BNCE=current.e("l", 0, rng=(0, 1))
-        )
+        , SPAC=current.e("eeo", 1, rng=(0,0.35))
+        , wght=current.e("seo", 1, rng=(0, 1))
+        , ital=current.e("qeo", 1, rng=(0, 1))
+        , INFM=current.e("seo", 1, rng=(0, 1))
+        , BNCE=current.e("l", 0, rng=(0, 1)))
         .lead(50)
         .xalign(f.a.r)
         .align(f.a.r)
         .f(0))
-
-release = languages.gifski()
